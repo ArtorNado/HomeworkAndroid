34526e405340145cca05964e2b38edaa47015907
artornado <16artem1999@mail.ru>
2019-10-22 20:29:06 +0300

    add homework

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,12 +1,34 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+const val REQUEST_CODE_SHARE = 1
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn_nint.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.putExtra(Intent.EXTRA_TEXT, tv_send.text)
+            intent.setType("text/plain")
+            startActivityForResult(intent, REQUEST_CODE_SHARE)
+        }
+    }
+
+    @SuppressLint("MissingSuperCall")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_CODE_SHARE && resultCode == Activity.RESULT_OK) {
+            Toast.makeText(this, "Shared", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Is Failed message", Toast.LENGTH_SHORT).show()
+        }
     }
 }
---
+++ app/src/main/java/com/example/myapplication/SecondActivity.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_second.*
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+        val intent: Intent = intent
+        val text = intent.extras.get(Intent.EXTRA_TEXT).toString()
+        tv_test.text = text
+    }
+}
